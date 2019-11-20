41bdb5b0cba5a685dff73b255823593b0e18dcee
gaosihe <1791568468@qq.com>
2019-11-20 21:59:20 +0800

    祝自己生日快乐

--- 敲代码/11月/1119/1119/1119.c
+++ 敲代码/11月/1119/1119/1119.c
@@ -1,84 +1,84 @@
-// #define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>//乘法表
-//#include<stdli b.h>
-//int main()
-//{
-//	int i=1, j=1,k;
-//	scanf("%d", &k);
-//	for (i = 1; i <= k; i++){
-//		for (j = 1; j <= i; j++)
-//		{
-//			printf("%d*%d=%d  ", i, j, i*j);
-//		}
-//		printf("\n");
-//	}
-//	
-// system("pause");
-// return 0;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>//使用函数交换两个变量
-//#include<stdlib.h>
-//int main()
-//{
-//	int a, b;
-//	scanf("%d%d",&a,&b);
-//	cha(&a, &b);
-//	printf("%d,%d",a,b );
-// system("pause");
-// return 0;
-//}
-//int cha(int* x,int *y)
-//{
-//	int tmp = *x;
-//	*x = *y;
-//	*y = tmp;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>//判断闰年
-//#include<stdlib.h>
-//int main()
-//{
-//	int a;
-//	scanf("%d", &a);
-//	leap(a);
-//	if (leap(a) == 1)
-//		printf("是闰年");
-//	else
-//		printf("不是闰年");
-// system("pause");
-// return 0;
-//}
-//int leap(int x)
-//{
-//	if (x % 4 == 0 && x % 100 != 0 || x % 400 == 0)
-//		return 1;
-//	else
-//		return 0;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>//判断素数
-//#include<stdlib.h>
-//#include<math.h>
-//int main()
-//{
-//	int c;
-//    int b,a;
-//	scanf("%d", &b);
-//	c = sqrt(b);
-//	for (a = 2; a <= c + 1; a++){
-//		if (b%a == 0){
-//			printf("%d不是素数", b);
-//			break;
-//		}
-//		else{
-//		}
-//	}
-//	if (a>c)
-//	printf("%d是素数", b);
-// system("pause");
-// return 0;
-//}
+ #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>//乘法表
+#include<stdli b.h>
+int main()
+{
+	int i=1, j=1,k;
+	scanf("%d", &k);
+	for (i = 1; i <= k; i++){
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%d  ", i, j, i*j);
+		}
+		printf("\n");
+	}
+	
+ system("pause");
+ return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>//使用函数交换两个变量
+#include<stdlib.h>
+int main()
+{
+	int a, b;
+	scanf("%d%d",&a,&b);
+	cha(&a, &b);
+	printf("%d,%d",a,b );
+ system("pause");
+ return 0;
+}
+int cha(int* x,int *y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>//判断闰年
+#include<stdlib.h>
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	leap(a);
+	if (leap(a) == 1)
+		printf("是闰年");
+	else
+		printf("不是闰年");
+ system("pause");
+ return 0;
+}
+int leap(int x)
+{
+	if (x % 4 == 0 && x % 100 != 0 || x % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>//判断素数
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+	int c;
+    int b,a;
+	scanf("%d", &b);
+	c = sqrt(b);
+	for (a = 2; a <= c + 1; a++){
+		if (b%a == 0){
+			printf("%d不是素数", b);
+			break;
+		}
+		else{
+		}
+	}
+	if (a>c)
+	printf("%d是素数", b);
+ system("pause");
+ return 0;
+}
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
---
+++ 敲代码/11月/1120/1120/1120.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+void main()
+{
+	int b, k = 4;
+	b = 200;
+	while (k)
+	{
+		system("color c");
+		printf("＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋ ＋\n");
+		printf("\t                     高嗣鹤\n\n");
+		printf("\t\t\t       * \n");
+		printf("\t\t\t       | \n");
+		printf("\t\t\t     ooooo \n");
+		printf("\t\t\t    | | | | \n");
+		printf("\t\t\tooooooooooooooo\n");
+		printf("\t\t\t|生 日 快 乐  |\n");
+		printf("\t\t\t|_____________|\n\n");
+		printf("\nHAPPY BIRTHDAY！\n\n");
+		printf("× × × × × × × × × × × × × × × × × × × × × × × × × × ×\n");
+		//Sleep(b);
+		system("cls");
+	}
+}
