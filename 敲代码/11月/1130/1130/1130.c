c82661974df1a5050133c80f217c0856978cdcfa
gaosihe <1791568468@qq.com>
2019-12-01 04:10:11 +0800

    三子棋完整版

--- 敲代码/11月/1130/1130/1130.c
+++ 敲代码/11月/1130/1130/1130.c
@@ -1,133 +1,91 @@
-//#define _CRT_SECURE_NO_WARNINGS//三子棋
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<time.h>
-//#define maxrow 3
-//#define  maxlin 3
-//void interface()	//开始界面
+#define _CRT_SECURE_NO_WARNINGS//三子棋
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#define maxrow 3
+#define  maxlin 3
+int interface()	//开始界面
+{
+	printf("欢迎来到三子棋游戏\n");
+	printf("===============\n");
+	printf("1.开始游戏\n");
+	printf("0.结束游戏\n");
+	printf("===============\n");
+	printf("请输入你的选择:\n");
+}
+char arr[maxrow][maxlin];
+void checkerboard()//初始化棋盘
+{
+	for (int row = 0; row < maxrow;row++)
+	{
+		for (int lin = 0; lin < maxlin;lin++)
+		{
+		  arr[row][lin] = ' ';
+		}
+	}
+}
+void print()//打印棋盘
+{
+	printf("\n+---+---+---+\n");
+	for (int row = 0; row < maxrow; row++)
+	{
+		for (int lin = 0; lin < maxlin; lin++)
+		{
+			printf("|  %c", arr[row][lin]);
+		}printf("| ");
+		printf("\n+---+---+---+\n");
+	}
+}
 //{
-//	printf("欢迎来到三子棋游戏\n");
-//	printf("===============\n");
-//	printf("1.开始游戏\n");
-//	printf("0.结束游戏\n");
-//	printf("===============\n");
-//	printf("请输入你的选择:\n");
-//}
-//void checkerboard(char arr [maxrow] [maxlin])
-//{
-//	int tmp;
-//	for (int row = 0; row < maxrow;row++)
-//	{
-//		for (int lin = 0; lin < maxlin;lin++)
-//		{
-//		  arr [row][lin] = ' ';
+//	for (int row = 0; row < maxrow; row++){
+//		printf("| %c | %c | %c |\n", arr[row][0], arr[row][1], arr[row][2]);
+//		if (row != maxrow- 1){
+//			printf("|---|---|---|\n");
 //		}
 //	}
 //}
-//void gamepalyer(char arr [maxrow] [maxlin])
-//{
-//
-//}
-//void game()
-//{
-//	char arr[maxrow][maxlin] = { 0 };
-//	checkerboard(arr [maxrow] [maxlin]);
-//	//初始化三乘三的棋盘
-//	while (1){
-//		gameplayer(arr);
-//		//玩家落子 判断游戏是否结束
-//		//电脑落子 判断游戏是否结束
-//
-//	}	
-//}
-//int main()
-//{
-//	srand((unsigned int)time(0));//随机种子
-//	interface();
-//	int choice;
-//	scanf("%d", &choice);
-//
-//	if (choice == 1)
-//	{
-//		game();
-//	}
-//	else if (choice == 0)
-//		printf("游戏结束!!!\n");
-//	else
-//		printf("你乱按是开始不了的哦\n");
-//	return choice;
-// system("pause");
-// return 0;
-//}						
-#define  _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define MAX_ROW 3
-#define MAX_COL 3
-int Menu(){
-	printf("=========================\n");
-	printf("1.开始游戏\n");
-	printf("0.结束游戏\n");
-	printf("=========================\n");
-	printf("请输入您的选择:");
-	int choose = 0;
-	scanf("%d", &choose);
-	return choose;
-}
-char chess_board[MAX_ROW][MAX_COL];
-void InitChess(){
-	for (int row = 0; row < MAX_ROW; row++){
-		for (int col = 0; col < MAX_COL; col++){
-			chess_board[row][col] = ' ';
-		}
-	}
-}
-void Print(){
-	for (int row = 0; row < MAX_ROW; row++){
-		printf("| %c | %c | %c |\n", chess_board[row][0], chess_board[row][1], chess_board[row][2]);
-		if (row != MAX_ROW - 1){
-			printf("|---|---|---|\n");
-		}
-	}
-}
-void MovePlayer(){
-	printf("玩家落子:\n");
+void gamepalyer()//玩家落子
+{
 	while (1){
-		printf("请输入一组坐标(row, col)\n");
-		int row = 0;
-		int col = 0;
-		scanf("%d %d", &row, &col);
-		if (row >= MAX_ROW || row < 0 || col >= MAX_ROW || col < 0){
-			printf("您的输入非法!\n");
+		printf("请输入你的棋子坐标:\n");
+		int row = 0, lin = 0;
+		scanf("%d %d", &row, &lin);
+		if (row >= maxrow || row < 0 || lin >= maxlin || lin < 0)
+		{
+			printf("你的输入有误!\n");
 			continue;
 		}
-		if (chess_board[row][col] != ' '){
-			printf("您输入的坐标已有棋子,请重新输入!\n");
+	    if (arr[row][lin]  != ' '){
+			printf("你输入的地方有子!\n");
 			continue;
 		}
-		chess_board[row][col] = 'x';
-		break;
+		arr[row][lin] = 'x';
+		break;		
 	}
 }
-void MoveCp(){
-	while (1){
-		int row = rand() % MAX_ROW;
-		int col = rand() % MAX_COL;
-		if (chess_board[row][col] != ' '){
-			continue;//已有棋子,重新随机
-		}
-		chess_board[row][col] = 'o';
-		break;
-	}
-}
-//函数返回1为棋盘满了,返回0为没满
-int IsFull(){
-	for (int row = 0; row <= MAX_ROW; row++){
-		for (int col = 0; col <= MAX_COL; col++){
-			if (chess_board[row][col] == ' '){
-				return 0;
+void computer()//电脑落子
+{
+	while (1)
+	{
+		int row = rand() % maxrow;
+		int lin = rand() % maxlin;
+			if (arr[row][lin] != ' '){
+				continue;
 			}
+				arr[row][lin] = 'o';
+			break;
+		}
+}	
+int judge()//判断游戏是否结束
+{
+	for (int row = 0; row < maxrow;row++)
+	{
+		for (int lin = 0; lin < maxlin;lin++)
+		{if (arr[row][lin]==' ')
+		{
+			return 0;
+		}
 		}
 	}
 	return 1;
@@ -136,74 +94,97 @@ int IsFull(){
 //返回'o'为电脑胜利
 //返回'q'为和局
 //返回' '继续游戏
-char CheckGameOver(){
-	for (int row = 0; row < MAX_ROW; row++){
-		if (chess_board[row][0] == chess_board[row][1] && chess_board[row][0] == chess_board[row][2]){
-			return chess_board[row][0];
+char CheckGameOver()//判断胜负
+{
+	for (int row = 0; row < maxrow; row++)
+	{
+		if (arr[row][0] == arr[row][1] && arr[row][0] == arr[row][2])
+		{
+			return arr[row][0];
 		}
 	}
-	for (int col = 0; col < MAX_COL; col++){
-		if (chess_board[0][col] == chess_board[1][col] && chess_board[0][col] == chess_board[2][col]){
-			return chess_board[0][col];
+	for (int lin = 0; lin < maxlin; lin++)
+	{
+		if (arr[0][lin] == arr[1][lin] && arr[0][lin] == arr[2][lin])
+		{
+			return arr[0][lin];
 		}
 	}
-	if (chess_board[0][0] == chess_board[1][1] && chess_board[0][0] == chess_board[2][2]){
-		return chess_board[0][0];
+	if (arr[0][0]==arr[1][1]&&arr[0][0]==arr[2][2])
+	{
+		return arr[0][0];
 	}
-	if (chess_board[0][2] == chess_board[1][1] && chess_board[0][2] == chess_board[2][0]){
-		return chess_board[0][2];
+	if (arr[2][0] == arr[1][1] && arr[2][0] == arr[0][2])
+	{
+		return arr[2][0];
 	}
-	if (IsFull()){
+	if (judge())
+	{
 		return 'q';
 	}
 	return ' ';
 }
-void Game(){
+void game()
+{
 	char winner = ' ';
-	InitChess();              //初始化棋盘
+	checkerboard();
+	//初始化三乘三的棋盘
 	while (1){
 		system("cls");
-		Print();                 //打印棋盘
-		MovePlayer();            //玩家落子
-		winner = CheckGameOver(); //判断胜负
-		if (winner != ' '){
-			break;                  //未分出胜负
+		print();
+		gamepalyer();//玩家落子 判断游戏是否结束
+		winner = CheckGameOver();
+		if (winner != ' ')
+		{
+			break;
 		}
-		MoveCp();                //电脑落子
-		winner = CheckGameOver(); //判断胜负
-		if (winner != ' '){
-			break;                  // 未分出胜负
+		computer();//电脑落子 判断游戏是否结束
+		winner = CheckGameOver();
+		if (winner != ' ')
+		{
+			break;
 		}
 	}
-	if (winner == 'x'){
-		system("cls");
-		printf("您胜利了!\n");
-		Print();
-	}
-	else if (winner == 'o'){
-		printf("弱爆了,电脑都下不过!\n");
-	}
-	else if (winner == 'q'){
-		printf("菜鸡,和电脑五五开!\n");
-	}
-	else{
-		printf("出错了~~~\n");
-	}
+		if (winner=='x')
+		{
+			system("cls");
+			printf("恭喜你赢了一个菜鸡!\n");
+			print();
+		}
+		else if (winner=='o')
+		{
+			printf("你比菜鸡还菜,竟然输了!\n");
+			print();
+		}
+		else if (winner=='q')
+		{
+			printf("你和菜鸡一样菜,打平了\n");
+			print();
+		}
+		else{
+			printf("出现bug,赶紧跑!!!");
+		}
 }
-int main(){
+int main()
+{
+	srand((unsigned int)time(0));//随机种子
 	while (1){
-		int choose = Menu();
-		if (choose == 1){
-			Game();
+		interface();
+		int choice = 0;
+		scanf("%d", &choice);
+	   
+		if (choice == 1)
+		{
+			game();
 		}
-		else if (choose == 0){
-			printf("goodbye!\n");
+		else if (choice == 0){
+			printf("游戏结束!!!\n");
 			break;
 		}
 		else{
-			printf("您的输入有误!\n");
+			printf("你乱按是开始不了的哦\n");
 		}
 	}
-	system("pause");
-	return 0;
+		system("pause");
+		return 0;	
 }
