48ff3d32da57c6c8bb59bca11b602098ebc8cd80
gaosihe <1791568468@qq.com>
2020-04-13 18:36:50 +0800

    练习

---
+++ idea/Testjava/src/Test0407/Test0413.java
@@ -0,0 +1,74 @@
+package Test0407;
+
+class Card implements Comparable<Card>{
+    public String rank;//点数
+    public String suit;//花色
+
+    public Card(String rank,String suit){
+        this.rank=rank;
+        this.suit=suit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //按照值来比较this和obj
+        if (this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        //obj这个类型是否为Card的类型
+        if(!(obj instanceof Card)){
+            return false;
+        }
+        //比较内容
+        Card other = (Card)obj;
+        return this.rank.equals(other.rank)&&this.suit.equals(other.suit);
+    }
+
+
+    @Override
+    public int compareTo(Card o) {
+        //如果this比o小返回小于<0
+        //===============大于0
+        //相等============,返回0
+        if(o==null){
+            return 1;
+        }
+        int rank1=this.getValue();
+        int rank2=o.getValue();
+        return rank1 - rank2;
+    }
+
+    private int getValue() {
+        int value = 0;
+        if ("J".equals(rank)){
+            value = 11;
+        }else if ("Q".equals(rank)){
+            value = 12;
+        }else if ("K".equals(rank)){
+            value = 13;
+        }else if ("A".equals(rank)){
+            value = 14;
+        }else {
+            value = Integer.parseInt(rank);
+        }
+        return value;
+    }
+}
+public class Test0413 {
+    public static void main(String[] args) {
+        Card p = new Card("3","♠");
+        Card q = new Card("3","♠");
+        Card o=p;
+//        System.out.println(p==o);
+//        System.out.println(p==q);
+//
+//        System.out.println("=============");
+//        System.out.println(p.equals(o));
+//        System.out.println(p.equals(q));
+        System.out.println(p.compareTo(q));
+        System.out.println(p.compareTo(o));
+    }
+}
