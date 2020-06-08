53a7f8da62294f8fff2a65a9e668b6d9e1c4d638
gaosihe <1791568468@qq.com>
2020-06-08 20:29:06 +0800

    common

---
+++ idea/io-study/src/Test1/dangao0608.java
@@ -0,0 +1,19 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class dangao0608 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int count = 0;
+        if (num1 % 4 == 0 || num2 % 4 == 0) {
+            count = num1 * num2 / 2;
+        } else {
+            count = num1 * num2 / 2 + 1;
+        }
+        System.out.println(count);
+    }
+}
