959e218914891ad9904275ed610138317a239118
gaosihe <1791568468@qq.com>
2020-06-10 20:12:48 +0800

    Test

---
+++ idea/io-study/src/Test2/Test0610.java
@@ -0,0 +1,25 @@
+package Test2;
+
+public class Test0610 {
+//    String str = new String("hello");
+//    char[] ch ={'a','b'};
+//
+//    public static void main(String[] args) {
+//        Test0610 test0610 = new Test0610();
+//        test0610.change(test0610.str,test0610.ch);
+//        System.out.println(test0610.str+"and");
+//        System.out.println(test0610.ch);
+//    }
+//    public void change(String str,char ch[]){
+//        str = "test ok";
+//
+//        ch[0]='c';
+//    }
+    private static void testMethod(){
+        System.out.println("testMethod");
+    }
+
+    public static void main(String[] args) {
+        ((Test0610)null).testMethod();
+    }
+}
