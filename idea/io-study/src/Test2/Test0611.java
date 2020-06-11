f9c0e5759f79e358796b28d9837a7766bef88db2
gaosihe <1791568468@qq.com>
2020-06-11 16:06:06 +0800

    机器人走网格

---
+++ idea/io-study/src/Test2/Test0611.java
@@ -0,0 +1,13 @@
+package Test2;
+public class Test0611 {
+    public static void main(String[] args) {
+        int x=2,y=3;
+        System.out.println(countWays(x ,y));
+    }
+        public static int countWays(int x, int y) {
+            if(x==1||y==1){
+                return 1;
+            }
+            return countWays(x-1,y)+countWays(x,y-1);
+    }
+}
