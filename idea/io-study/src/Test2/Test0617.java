ce298bf9a2b7423f45ed389dbf4e44fa17008ef7
gaosihe <1791568468@qq.com>
2020-06-17 13:35:06 +0800

    Test

---
+++ idea/io-study/src/Test2/Test0617.java
@@ -0,0 +1,26 @@
+package Test2;
+
+import java.util.Scanner;
+
+public class Test0617 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      int[] count = new int[20];
+      for (int i = 0;i<10 ;i++){
+          count[i] = sc.nextInt();
+      }
+      for (int i = 1;i<10;i++){
+          if (count[i]>0){
+              System.out.print(i);
+              count[i]--;
+              break;
+          }
+      }
+        for (int i = 0;i<10;i++){
+            while (count[i]>0){
+                System.out.print(i);
+                count[i]--;
+            }
+        }
+    }
+}
---
+++ idea/io-study/src/Test2/Tests0617.java
@@ -0,0 +1,39 @@
+package Test2;
+
+import java.util.Scanner;
+
+public class Tests0617 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input =sc.nextInt();
+        sc.close();
+        int n =input*input*input;
+        System.out.println(getString(n,input));
+    }
+
+    private static String getString(int n, int input) {
+        int flag = 0;
+        outer:
+        for (int i = 1;i<=n;i++){
+            int temp = i;
+            int sum = 0;
+            flag = temp;
+            for (int j = 0;j <input;j++){
+                sum += temp;
+                if (sum==n){
+                    break outer;
+                }
+                temp+=2;
+            }
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0;i<input;i++){
+            if (i<input - 1){
+                stringBuffer.append(flag).append("+");
+                flag+=2;
+            }
+        }
+        stringBuffer.append(flag);
+        return stringBuffer.toString();
+    }
+}
