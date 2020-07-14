779b81c4b2feefc47a11cf01394f4cdc01aff744
gaosihe <1791568468@qq.com>
2020-07-14 18:32:23 +0800

    Test

---
+++ idea/stu-dorm-sys/src/test/java/util/DBUtilTest.java
@@ -0,0 +1,11 @@
+package util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DBUtilTest {
+    @Test
+    public void test(){
+        Assert.assertNotNull(DBUtil.getConnection());
+    }
+}
