4fef2f5084d4b0a80e5d66e35bc30645478fb9a7
gaosihe <1791568468@qq.com>
2020-07-17 19:34:45 +0800

    model

---
+++ idea/stu-dorm-sys/src/main/java/model/Page.java
@@ -0,0 +1,28 @@
+package model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Getter
+@Setter
+@ToString
+public class Page {
+    private String searchText;//搜索的内容
+    private String sortOrder;//排序方式
+    private Integer pageSize;//页数
+    private Integer pageNumber;//当前页码
+
+    public static Page parse(HttpServletRequest req){
+        Page p= new Page();
+        p.searchText = req.getParameter("searchText");
+        p.sortOrder = req.getParameter("sortOrder");
+        p.pageSize = Integer.parseInt(req.getParameter("pageSize"));
+        p.pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
+        return p;
+    }
+}
