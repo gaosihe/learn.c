611cb9fbdeac30a97047f67ccd93b6db617f213b
gaosihe <1791568468@qq.com>
2020-07-26 19:58:05 +0800

    test0726

---
+++ idea/exercises-test-study/src/Test/Test5/MaxDifferenceValue0726.java
@@ -0,0 +1,17 @@
+package Test.Test5;
+
+public class MaxDifferenceValue0726 {
+        public int findMaxGap(int[] A, int n) {
+            int max = 0;
+            for (int i = 0; i < A.length; i++) {          //找出数组中的最大值
+                if (A[i] > max)
+                    max = A[i];
+            }
+            int ans1 = max - A[0];
+            int ans2 = max - A[n - 1];
+            if (ans1 > ans2)
+                return ans1;
+            else
+                return ans2;
+        }
+}
