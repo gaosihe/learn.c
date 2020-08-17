1a5f01e3c688c70a8897f8908daa217d42db36d7
gaosihe <1791568468@qq.com>
2020-08-17 21:54:48 +0800

    springboot-study/src/main/java/frank/config/TestLifecycle2.java

---
+++ idea/javaee-study/springboot-study/src/main/java/frank/config/TestLifecycle2.java
@@ -0,0 +1,31 @@
+package frank.config;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestLifecycle2 implements
+        BeanPostProcessor
+{
+    /**
+     * BeanFactoryAware接口方法
+     * @param beanFactory
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization"+beanName);
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization"+beanName);
+        return null;
+    }
+}
