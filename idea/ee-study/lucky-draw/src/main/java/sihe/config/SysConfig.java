fed1bcd51e98c18160996da4dac06622e2e829a7
gaosihe <1791568468@qq.com>
2020-08-22 22:17:29 +0800

    Test

--- idea/ee-study/lucky-draw/src/main/java/sihe/config/SysConfig.java
+++ idea/ee-study/lucky-draw/src/main/java/sihe/config/SysConfig.java
@@ -3,6 +3,7 @@ package sihe.config;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
@@ -18,6 +19,10 @@ public class SysConfig implements WebMvcConfigurer, InitializingBean{
     @Resource
     private RequestMappingHandlerAdapter adapter;
 
+
+
+    //和之前以@ControllerAdvice+实现ResponseBodyAdvice接口,完成统一处理返回数据包装:无法解决返回null包装为自定义类型
+    //改用现在这种方式,可以解决返回null包装为自定义类型
     @Override
     public void afterPropertiesSet() {
         List<HandlerMethodReturnValueHandler> returnValueHandlers = adapter.getReturnValueHandlers();
@@ -29,8 +34,16 @@ public class SysConfig implements WebMvcConfigurer, InitializingBean{
             }
         }
         adapter.setReturnValueHandlers(handlers);
+
     }
 
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        //Controller 路径,统一添加请求的路径前缀,第二个参数,表示是否添加
+        //表示所有Controller路径加/api前缀
+        configurer.addPathPrefix("api",c->true);
+
+    }
 }
 
 
