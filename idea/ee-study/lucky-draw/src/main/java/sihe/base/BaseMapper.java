e38e80d471fa03ded0697b46757b24e75ee89a45
gaosihe <1791568468@qq.com>
2020-08-22 22:07:11 +0800

    test

--- idea/ee-study/lucky-draw/src/main/java/sihe/base/BaseMapper.java
+++ idea/ee-study/lucky-draw/src/main/java/sihe/base/BaseMapper.java
@@ -1,4 +1,25 @@
 package sihe.base;
 
+import java.util.List;
+
 public interface BaseMapper<T extends BaseEntity> {
+    T selectByPrimaryKey (Integer id);//查询单个奖品
+
+    int deleteByPrimaryKey(Integer id);//删除指定奖品
+
+    List<T> selectAll();
+
+    List<T> selectByCondition(T t);
+
+    int insert(T record);
+
+    int insertSelective(T record);
+
+    int updateByPrimaryKeySelective(T record);
+
+    int updateByPrimaryKey(T record);
+
+    T selectOne(T record);
+
+    int deleteByIds(List<Integer> ids);
 }
