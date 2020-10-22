31b0182e1604cb8a84affd89ac1034f6d51c1a1a
gaosihe <1791568468@qq.com>
2020-10-22 16:21:44 +0800

    Test

---
+++ idea/exercises-test-study/src/Test/Test7/bianlifeng1010.java
@@ -0,0 +1,22 @@
+package Test.Test7;
+
+import java.util.Scanner;
+
+public class bianlifeng1010 {
+    static int m;
+    static int k;
+    static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            m = sc.nextInt();
+            k = sc.nextInt();
+            arr = new int[k];
+            return ;
+        }
+    }
+
+    private static void computer(int t, int n) {
+
+    }
+}
